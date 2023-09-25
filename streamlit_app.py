9c15779b003b8229887add549d473674d2c5ca62
cnegrao <cnegrao2@gmail.com>
2023-09-25 09:55:12 -0300

    Update streamlit_app.py
    pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,12 @@
 import streamlit
+import pandas
+
 streamlit.title('altamente mais ou menos')
 streamlit.header('header property')
 streamlit.text('text property')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.text('testa refresh')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
