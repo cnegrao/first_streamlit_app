e1c11d6b5578c682efd9005f5cbf29cac84a4b32
cnegrao <cnegrao2@gmail.com>
2023-09-27 15:14:13 -0300

    Update streamlit_app.py
    data 2 ffood 3

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,15 +3,12 @@ import pandas
 import requests
 
 
-streamlit.text(fruityvice_response)
 streamlit.title('altamente mais ou menos')
 streamlit.header('header property')
 streamlit.text('text property')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.text('testa refresh')
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -22,7 +19,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response.json())
 
 
 
