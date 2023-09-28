3c85794bee3c6ee1262b1260a18bab19bc970591
cnegrao <cnegrao2@gmail.com>
2023-09-27 23:09:40 -0300

    Update streamlit_app.py
    jjjjjjjjjjjk

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ streamlit.dataframe(my_data_rows)
 
 fruit_add = streamlit.text_input('What fruit would you like add?','jaca')
 streamlit.write('The user entered ', fruit_add)
-my_cur.execute("insert into fruit_load_lists values" + fruit_add)
+my_cur.execute("insert into fruit_load_lists values " + fruit_add)
 
 streamlit.title('altamente mais ou menos')
 streamlit.header('DENAO CARARECO COISADO')
