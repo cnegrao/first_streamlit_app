c500f467df9c02823ac9fd7648504ea12d43e069
cnegrao <cnegrao2@gmail.com>
2023-09-28 13:48:12 -0300

    Update streamlit_app.py
    fdfd

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,19 +33,19 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 try:
-streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-if not fruit_choice:
-  streamlit.error("erro escolha")
-else:
-  streamlit.write('The user entered ', fruit_choice)
-  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-  streamlit.text(fruityvice_response)
-  # write your own comment -what does the next line do? 
-  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()) 
-  #streamlit.text(fruityvice_response.json())
-  # write your own comment - what does this do?
-  streamlit.dataframe(fruityvice_normalized)
+  streamlit.header("Fruityvice Fruit Advice!")
+  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  if not fruit_choice:
+    streamlit.error("erro escolha")
+  else:
+    streamlit.write('The user entered ', fruit_choice)
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    streamlit.text(fruityvice_response)
+    # write your own comment -what does the next line do? 
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()) 
+    #streamlit.text(fruityvice_response.json())
+    # write your own comment - what does this do?
+    streamlit.dataframe(fruityvice_normalized)
 except URLError as e:
   streamlit.error()
 
